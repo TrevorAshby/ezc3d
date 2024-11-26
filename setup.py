92ea6595e62fa5dbef45035abf197723fd7e0fdb
Trevor Ashby <trevor.clark.ashby@gmail.com>
2024-11-26 18:30:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ try:
 except(IOError, ImportError):
     long_description = open(f"{dir_path}/README.md").read()
 
-with open(f"{dir_path}/CMakeLists.txt") as file:
-    for line in file:
-        match = re.search(re.compile("project\\(ezc3d VERSION (\\d*\\.\\d*\\.\\d*)\\)"), line)
-        if match is not None:
-            version = match[1]
-            break
-    else:
-        raise RuntimeError("Version not found")
+# with open(f"{dir_path}/CMakeLists.txt") as file:
+#     for line in file:
+#         match = re.search(re.compile("project\\(ezc3d VERSION (\\d*\\.\\d*\\.\\d*)\\)"), line)
+#         if match is not None:
+#             version = match[1]
+#             break
+#     else:
+#         raise RuntimeError("Version not found")
 
 # Create an empty 'ezc3d' folder if it doesn't exist (stub for setup.py)
 Path("ezc3d").mkdir(exist_ok=True)
